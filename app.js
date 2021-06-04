36f6cfdfd408ab4edc6edf030dfa74b5b1f4123d
hannagustinvega <hannheitiragustin@hotmail.com>
2021-06-04 01:12:07 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+
+
+
+app.listen(port, () => console.log("Servidor corriendo en el " + port));
